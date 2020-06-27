a859ed0e907cc516ea26eabe2f68944d0fd58503
AnuranDutta <anuran_201700244@smit.smu.edu.in>
2020-06-28 00:00:21 +0530

    Template Setup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/static'));
 app.set('view-engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index.ejs')
