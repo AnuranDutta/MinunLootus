226d539df0af416df22ea90b075cf23173ab9b5f
AnuranDutta <anuran_201700244@smit.smu.edu.in>
2020-06-27 23:28:19 +0530

    server setup

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+app.set('view-engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+});
 
 app.listen(PORT, () => console.log (`Listening to port ${PORT}`));
